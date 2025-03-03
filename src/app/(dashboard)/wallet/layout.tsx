cad5cc3d58b8eac4143513c9e1fe5811d0d69b19
ArtemBusyhin <a.busyhin@urancompany.com>
2025-03-03 08:43:06 +0100

    remove middleware

--- src/app/(dashboard)/wallet/layout.tsx
+++ src/app/(dashboard)/wallet/layout.tsx
@@ -1,4 +1,8 @@
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+import { APP_PATH } from "@/lib/constants";
 
 type Props = {
   index: React.ReactNode;
@@ -6,6 +10,10 @@ type Props = {
 };
 
 export default async function DashboardLayout({ address, index }: Readonly<Props>) {
+  const session = await auth();
+
+  if (!session) redirect(APP_PATH.SIGN_IN);
+
   const cookieStore = cookies();
   const hasAddressData = (await cookieStore).get("address")?.value;
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,43 @@
-// import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import type { NextRequest } from "next/server";
 
-import { auth } from "./lib/auth";
-import { Session } from "next-auth";
+// import { auth } from "./lib/auth";
+// import { Session } from "next-auth";
 
-// export async function middleware(req: NextRequest) {
-//   const session = await auth();
+export async function middleware() {
+  // const session = await auth();
 
-//   if (!session) {
-//     return NextResponse.redirect(new URL("/sign-in", req.url));
+  // if (!session) {
+  //   return NextResponse.redirect(new URL("/sign-in", req.url));
+  // }
+
+  return NextResponse.next();
+}
+
+// const protectedRoutes = ["/wallet"];
+
+// export default auth(async function middleware(req: NextRequest & { auth: Session | null }) {
+//   const { pathname } = req.nextUrl;
+
+//   const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
+
+//   if (isProtected) {
+//     const session = req.auth;
+
+//     if (!session) {
+//       const signInUrl = new URL("/sign-in", req.url);
+//       signInUrl.searchParams.set("callbackUrl", pathname);
+//       return NextResponse.redirect(signInUrl);
+//     }
+
+//     console.log(`Authenticated request to ${pathname} by ${session.user?.email}`);
 //   }
 
 //   return NextResponse.next();
-// }
+// });
 
-const protectedRoutes = ["/wallet"];
-
-export default auth(async function middleware(req: NextRequest & { auth: Session | null }) {
-  const { pathname } = req.nextUrl;
-
-  const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
-
-  if (isProtected) {
-    const session = req.auth;
-
-    if (!session) {
-      const signInUrl = new URL("/sign-in", req.url);
-      signInUrl.searchParams.set("callbackUrl", pathname);
-      return NextResponse.redirect(signInUrl);
-    }
-
-    console.log(`Authenticated request to ${pathname} by ${session.user?.email}`);
-  }
-
-  return NextResponse.next();
-});
-
-export const config = {
-  matcher: ["/wallet/:path*"],
-  // matcher: ["/wallet(.*)"],
-};
+// export const config = {
+//   matcher: ["/wallet(.*)"],
+// };
 
 // export const runtime = "nodejs";
